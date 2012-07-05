7baf6fd4adbaafea8a2fd605d48ffbea8a646f05
induk <indirakorrapati@gmail.com>
2012-07-05 17:03:45 -0400

    Removed default Rails page

--- autotest/discover.rb
+++ autotest/discover.rb
@@ -1,2 +1,8 @@
 Autotest.add_discovery { "rails" }
 Autotest.add_discovery { "rspec2" }
+Autotest.add_hook : initial do |autotest|
+autotest.add_mapping(/^spec\/requests\/.*_spec\.rb$/) do
+autotest.files_matching(/^spec\/requests\/.*spec\.rb$/)
+end
+end
+
